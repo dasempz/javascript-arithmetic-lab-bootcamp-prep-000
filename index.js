42b0b4b60cb098f13527758c631648d906a475dc
Andrea H <andhaveman@hotmail.com>
2019-03-11 15:36:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,48 @@
+function add (a, b){
+  return a + b;
+}
+function subtract (a, b){
+  return a - b;
+}
+function multiply (a, b){
+  return a * b;
+}
+function divide (a, b){
+  return a / b;
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+  }
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
+
+
+
+/*function makeInt(n){
+  parseFloat('10', 10);
+  return n;
+}
+
+function preserveDecimal(){
+  n = parseFloat('2.222');
+  return n;
+}
+
+*/
+
+
+
